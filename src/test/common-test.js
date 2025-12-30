31a3d190f002843264fa319d95c23d5c2f1b1f69
Krzysztof Ruta <krzysztof.ruta@play.pl>
2025-12-30 11:49:57 +0100

    improve tests, add simple test for scene rotation

--- src/test/common-test.js
+++ src/test/common-test.js
@@ -4,12 +4,12 @@ export function assertEquals(actual, expected, message) {
     }
 }
 
-export function assertEqualsRounded(actual, expected, precissionDigits, message) {
-    let factor = 10^precissionDigits;
+export function assertEqualsRounded(actual, expected, precisionDigits, message) {
+    let factor = 10^precisionDigits;
     let actualRounded = Math.round(actual * factor) / factor;
     let expectedRounded = Math.round(expected * factor) / factor;
     if (actualRounded !== expectedRounded) {
-        throw new Error(`Assertion failed with message: ${message}. Expected: ${expected}. Got: ${actual} (precission: ${precissionDigits} digits`);
+        throw new Error(`Assertion failed with message: ${message}. Expected: ${expected}. Got: ${actual} (precission: ${precisionDigits} digits`);
     }
 }
 
@@ -19,6 +19,22 @@ export function assertTrue(condition, message) {
 
 export function assertFalse(condition, message) {
     assertEquals(condition, false, message);
+}
+
+export function assertEqualMatrices(actual, expected, precisionDigits, message) {
+    assertEquals(actual.length, expected.length, 'Matrices should have the same number of rows');
+    for(let row = 0; row < actual.length; row++) {
+        assertEquals(actual[row].length, expected[row].length, `Matrices should have the same number of columns in each row (row ${row}`);
+        for(let col = 0; col < actual[row].length; col++) {
+            assertEqualsRounded(actual[row][col], expected[row][col], precisionDigits, `${message} (row ${row} col ${col})`);
+        }
+    }
+}
+
+export function assertEqualCoords(actual, expected, precisionDigits, message) {
+    assertEqualsRounded(actual.x, expected.x, precisionDigits, `${message} (x)`);
+    assertEqualsRounded(actual.y, expected.y, precisionDigits, `${message} (y)`);
+    assertEqualsRounded(actual.z, expected.z, precisionDigits, `${message} (z)`);
 }
 
 export function runTest(test) {
--- src/test/geometry-test.js
+++ src/test/geometry-test.js
@@ -1,6 +1,7 @@
 import {Point3D} from "../main/geometry.js";
 import {Scene} from "../main/scene.js";
-import {assertEqualsRounded, runTest} from "./common-test.js";
+import {assertEqualCoords, assertEqualMatrices, assertEqualsRounded, runTest} from "./common-test.js";
+import {CubeCoords} from "../main/cube.js";
 
 function testPoint3DRotation() {
     const point = new Point3D(1, 2, 3);
@@ -11,19 +12,29 @@ function testPoint3DRotation() {
     const rotationCenter = new Point3D(0, 0, 0);
 
     const pointX90 = point.clone().rotate(rotationMatrixX90, rotationCenter);
-    assertEqualsRounded(pointX90.x, 1, 2, 'X should not change');
-    assertEqualsRounded(pointX90.y, -3, 2, 'Y should have a value of -Z');
-    assertEqualsRounded(pointX90.z, 2, 2, 'Z should should have a value of Y');
+    assertEqualCoords(pointX90, { x: 1, y: -3, z: 2 }, 2,
+        'X should not change, Y should have a value of -Z, Z should should have a value of Y');
 
     const pointY90 = point.clone().rotate(rotationMatrixY90, rotationCenter);
-    assertEqualsRounded(pointY90.x, 3, 2, 'X should have a value of Z');
-    assertEqualsRounded(pointY90.y, 2, 2, 'Y should not change');
-    assertEqualsRounded(pointY90.z, -1, 2, 'Z should should have a value of -X');
+    assertEqualCoords(pointY90, { x: 3, y: 2, z: -1 }, 2,
+        'X should have a value of Z, Y should not change, Z should should have a value of -X');
 
     const pointZ90 = point.clone().rotate(rotationMatrixZ90, rotationCenter);
-    assertEqualsRounded(pointZ90.x, -2, 2, 'X should have a value of -Y');
-    assertEqualsRounded(pointZ90.y, 1, 2, 'X should have a value of X');
-    assertEqualsRounded(pointZ90.z, 3, 2, 'Z should not change');
+    assertEqualCoords(pointZ90, { x: -2, y: 1, z: 3 }, 2,
+        'X should have a value of -Y, Y should have a value of X, Z should not change');
+}
+
+function testSceneRotation() {
+    const scene = new Scene();
+
+    assertEqualMatrices(scene.rotationMatrix, [ [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ] ], 5, 'Initial rotation matrix with 1s on diagonal');
+    scene.rotate(90 ,90, 90);
+    assertEqualMatrices(scene.rotationMatrix, [ [ 0, 0, 1 ], [ 0, 1, 0 ], [ -1, 0, 0 ] ], 'Axis are switched after rotation');
+}
+
+function testCubeCoordsRotation() {
+    const cubeCoords = new CubeCoords(1,1,1);
 }
 
 runTest(testPoint3DRotation);
+runTest(testSceneRotation);
